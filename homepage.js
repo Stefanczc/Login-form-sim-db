bf02a4edef4edefb93a68bb1305545522f0c0a9e
Stefan Alexandru <stefanalecsandru@yahoo.com>
2023-09-08 12:23:33 +0300

    added new stuff

--- homepage.js
+++ homepage.js
@@ -1,11 +1,11 @@
-const info = document.getElementById('info');
+import { apiKey } from "./stuff.js";
 
 async function fetchData() {
   const url = 'https://jobsearch4.p.rapidapi.com/api/v2/Jobs/Latest';
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'ab2a8cf537msh5dd281f7f69058ep1d9312jsnc2690e711eb8',
+      'X-RapidAPI-Key': apiKey,
       'X-RapidAPI-Host': 'jobsearch4.p.rapidapi.com'
     }
   };
@@ -14,13 +14,11 @@ async function fetchData() {
     const response = await fetch(url, options);
 
     if (response.ok) {
-      const result = await response.json(); // Parse the JSON data
-      console.log(result.data); // This will display the parsed JSON object
-      const company = result.data[0].company;
-      console.log(company);
+      const result = await response.json(); 
+      console.log(result.data); 
 
-      // Update the innerText of the 'info' element
-      info.innerText = company;
+      // Call the new method to iterate through the data array
+      processDataArray(result.data);
     } else {
       throw new Error(`Request failed with status: ${response.status}`);
     }
@@ -29,5 +27,21 @@ async function fetchData() {
   }
 }
 
-// Call the fetchData function to initiate the asynchronous operation
+const listOfJobs = document.getElementById('listOfJobs');
+
+function processDataArray(dataArray) {
+  dataArray.forEach((item) => {
+    const job = document.createElement('li');
+    const titleJob = document.createElement('h3');
+    titleJob.innerText = item.title;
+    const companyJobName = document.createElement('p');
+    companyJobName.innerText = item.company;
+
+    job.appendChild(titleJob);
+    job.appendChild(companyJobName);
+    listOfJobs.appendChild(job);
+  
+  });
+}
+
 fetchData();
